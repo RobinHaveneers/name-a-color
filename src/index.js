8963a74de2451fe2dbb034016fa43a8e3b9c6db4
Robin Haveneers <haveneersrobin@gmail.com>
2017-02-28 15:32:40 +0100

    Basic functionality added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 // @flow
 
-export default {
-  add: (a: number, b: number) => a + b,
-  subtract: (a: number, b: number) => a - b,
+const names = require('color-names');
+const converter = require('color-convert');
+
+const difference = (l1, l2) =>
+  l1.map((e, i) => (e - l2[i]) ** 2).reduce((a, b) => a + b);
+
+const error = (hex: string, hox: string): number => {
+  const errorRGB = difference(converter.hex.rgb(hex), converter.hex.rgb(hox));
+  const errorHSL = difference(converter.hex.hsl(hex), converter.hex.hsl(hox));
+  return errorRGB + (errorHSL * 2);
+};
+
+const findHexName = (hex: string): ?string => {
+  let min;
+  Object.keys(names)
+        .map(e => ({ error: error(e.substr(1), hex), name: names[e] }))
+        .forEach((e) => {
+          if (!min || e.error < min.error) { min = e; }
+        });
+  return min ? min.name : undefined;
+};
+
+module.exports = {
+  findHexName,
 };
